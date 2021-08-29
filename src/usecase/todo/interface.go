1516b4dc77c9dbb6e23426752606254c95814dec
Heggies Iqbal <heggies0iqbal@gmail.com>
2021-08-29 15:39:48 +0700

    just me being drunk kebolak nama interfacenya 🍻🍻

--- src/usecase/todo/interface.go
+++ src/usecase/todo/interface.go
@@ -2,17 +2,17 @@ package todo
 
 import "github.com/heggies/todo-server/src/entity/todo"
 
-type Writer interface {
+type Reader interface {
 	Get() (entities []todo.Todo, err error)
 }
 
-type Reader interface {
+type Writer interface {
 	Create(entity todo.Todo) (todo.Todo, error)
 	Update(entity todo.Todo) (todo.Todo, error)
 	Delete(id int) (err error)
 }
 
 type Repositorier interface {
-	Writer
 	Reader
+	Writer
 }
